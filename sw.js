40b87d1d35a8dec2752a22ed5777ee336a543561
ordermissshop-ui <ordermissshop@gmail.com>
2026-01-09 21:28:29 +1100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,44 +1,40 @@
-const CACHE_NAME = kids-todo-pwa-v1;
+const CACHE_NAME = "kids-todo-pwa-v2";
 const ASSETS = [
-  .,
-  .index.html,
-  .style.css,
-  .app.js,
-  .manifest.webmanifest,
-  .iconsicon-192.png,
-  .iconsicon-512.png
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener(install, (event) = {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) = cache.addAll(ASSETS))
-  );
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener(activate, (event) = {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =
-      Promise.all(keys.map((k) = (k !== CACHE_NAME  caches.delete(k)  null)))
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener(fetch, (event) = {
-  const req = event.request;
-
-   Cache-first for our app files
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(req).then((cached) = {
+    caches.match(event.request).then((cached) => {
       if (cached) return cached;
 
-      return fetch(req).then((res) = {
-         Save a copy of fetched files
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then((cache) = cache.put(req, copy));
-        return res;
-      }).catch(() = caches.match(.index.html));
+      return fetch(event.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return res;
+        })
+        .catch(() => caches.match("./index.html"));
     })
   );
 });

fac69f3d3f3034286b99b7c07ae4963ef2cc0dd8
ordermissshop-ui <ordermissshop@gmail.com>
2026-01-09 20:53:18 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = kids-todo-pwa-v1;
+const CACHE_NAME = kids-todo-pwa-v2;
 const ASSETS = [
   .,
   .index.html,
@@ -42,3 +42,4 @@ self.addEventListener(fetch, (event) = {
     })
   );
 });
+
